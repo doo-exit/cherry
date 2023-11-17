5fa43d460d78fa617cc11402f03fbda7a2651097
은정 Rachel <50548458+iamkkwak@users.noreply.github.com>
2023-11-17 15:59:12 +0900

    https://leetcode.com/problems/minimum-size-subarray-sum/

---
+++ 2023-11-13/minSubArrayLen.ts
@@ -0,0 +1,24 @@
+const MAX_LENGTH = Math.pow(10, 5) + 1;
+
+function minSubArrayLen(target: number, nums: number[]): number {
+  let answer = MAX_LENGTH;
+  
+  let left = 0, right = 0, count = 1, currentSum = nums[0];
+
+  while (right < nums.length) {
+    if (left <= right && target <= currentSum) {
+      if (count < answer) {
+        answer = count;
+      }
+      currentSum -= nums[left];
+      left += 1;
+      count -= 1;
+    } else {
+      right += 1;
+      count += 1;
+      currentSum += nums[right];
+    }
+  }
+
+  return answer === MAX_LENGTH ? 0 : answer;
+};
