d6ff2e4645f36b0b764fb29b7cd0b8fa2f8edc4a
은정 Rachel <50548458+iamkkwak@users.noreply.github.com>
2024-01-27 15:21:52 +0900

    https://leetcode.com/problems/valid-anagram/

---
+++ 2024-01-22/isAnagram.ts
@@ -0,0 +1,16 @@
+function isAnagram(s: string, t: string): boolean {
+    if (s.length !== t.length) {
+        return false;
+    }
+    
+    const sortedS = Array.from(s).sort();
+    const sortedT = Array.from(t).sort();
+
+    for (let i = 0; i < s.length; i++) {
+        if (sortedS[i] !== sortedT[i]) {
+            return false;
+        }
+    }
+
+    return true;
+};
